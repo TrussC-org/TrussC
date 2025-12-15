0d85f6315217853766807e0404550b65de1aa01e
tettou771 <tettou771@gmail.com>
2025-12-15 22:51:35 +0900

    fix: サンプルの pixelPerfect を false に統一
    - Retina環境でビットマップフォントが読みやすいよう論理座標を使用

--- examples/04_color/src/main.cpp
+++ examples/04_color/src/main.cpp
@@ -2,7 +2,6 @@
 
 int main() {
     tc::WindowSettings settings;
-    settings.setPixelPerfect(true);
     settings.setSize(1280, 720);
     settings.setTitle("04_color - Color Space Demo");
     return tc::runApp<tcApp>(settings);
--- examples/05_3d_primitives/src/main.cpp
+++ examples/05_3d_primitives/src/main.cpp
@@ -2,7 +2,6 @@
 
 int main() {
     tc::WindowSettings settings;
-    //settings.setPixelPerfect(true);
     settings.setSize(1280, 720);
     settings.setTitle("05_3d_primitives - 3D Primitives Demo");
     return tc::runApp<tcApp>(settings);
