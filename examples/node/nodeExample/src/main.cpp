e3179270e1ed8d2cb2e816982f6a09e297938d45
tettou771 <tettou771@gmail.com>
2025-12-25 23:03:35 +0900

    refactor: reorganize example categories
    - Move imguiExample from gui/ to ImGui/
    - Move hitTestExample, uiExample from events/ to node/
    - Move ofNodeExample from 3d/ to node/nodeExample

--- examples/node/nodeExample/src/main.cpp
+++ examples/node/nodeExample/src/main.cpp
@@ -3,6 +3,6 @@
 int main() {
     WindowSettings settings;
     settings.setSize(960, 600);
-    settings.setTitle("02_nodes - Node System Demo");
+    settings.setTitle("nodeExample - Node System Demo");
     return runApp<tcApp>(settings);
 }
