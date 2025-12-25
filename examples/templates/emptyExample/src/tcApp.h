e0fb6d33de9007347f7c8c3156522af33a3f5d02
tettou771 <tettou771@gmail.com>
2025-12-25 13:05:44 +0900

    refact: emptyExampleをスッキリ

--- examples/templates/emptyExample/src/tcApp.h
+++ examples/templates/emptyExample/src/tcApp.h
@@ -1,6 +1,5 @@
 #pragma once
-
-#include "tcBaseApp.h"
+#include "TrussC.h"
 using namespace tc;
 using namespace std;
 
@@ -10,23 +9,16 @@ public:
     void update() override;
     void draw() override;
 
-    // Key events
     void keyPressed(int key) override;
     void keyReleased(int key) override;
 
-    // Mouse events
     void mousePressed(Vec2 pos, int button) override;
     void mouseReleased(Vec2 pos, int button) override;
     void mouseMoved(Vec2 pos) override;
     void mouseDragged(Vec2 pos, int button) override;
     void mouseScrolled(Vec2 delta) override;
 
-    // Window events
     void windowResized(int width, int height) override;
-
-    // Exit
-    void exit() override;
-
-    // File drop
     void filesDropped(const vector<string>& files) override;
+    void exit() override;
 };
