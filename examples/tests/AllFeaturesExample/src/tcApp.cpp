cf005a2f6bfdce1d47c2cc7c6f9b30dab3dae656
tettou771 <tettou771@gmail.com>
2026-04-18 17:57:45 +0900

    add: Rect::getCenter() returning Vec2

--- examples/tests/AllFeaturesExample/src/tcApp.cpp
+++ examples/tests/AllFeaturesExample/src/tcApp.cpp
@@ -127,7 +127,7 @@ void tcApp::setup() {
     tc::Font fontTest;
     tc::Node nodeTest;
     tc::Rect rectTest(0, 0, 100, 100);
-    rectTest.getCenterX();
+    rectTest.getCenter();
 
     tc::logNotice("AllFeaturesExample") << "Animation/types test completed";
 
